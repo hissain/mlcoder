ba6827ec929364fc7ad8966f22e40b6074f23be8
Sazzad Hissain Khan <hissain.khan@gmail.com>
2025-03-12 22:11:00 +0600

    Updated

--- mlcoder/file.py
+++ mlcoder/file.py
@@ -2,20 +2,15 @@
 
 import os
 import shutil
+import inspect
 
 def copy_file_to_working_directory(filename, filedir='files'):
-    """
-    Copies a file from the specified folder to the directory of the caller script.
-
-    Parameters:
-    filename (str): The name of the file to copy.
-    filedir (str): The directory where the source file is located.
-    """
     source_path = os.path.join(os.path.dirname(__file__), filedir, filename)
     print(source_path)
 
-    # Get the directory of the caller script
-    caller_directory = os.path.dirname(os.path.abspath(__file__))
+    caller_frame = inspect.stack()[1]
+    caller_file = caller_frame.filename
+    caller_directory = os.path.dirname(os.path.abspath(caller_file))
     destination_path = os.path.join(caller_directory, filename)
     print(destination_path)
     
